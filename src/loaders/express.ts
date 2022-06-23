3d449f95bd30eeba850e7baf3849a3b30f1a7e9c
almog1her <almog1her@gmail.com>
2022-06-23 20:05:39 +0300

    Fix auto update

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -31,6 +31,7 @@ export default (app: Application): void => {
 
   const whitelist = [
     'http://localhost',
+    'http://localhost:3000',
     'http://127.0.0.1',
     'http://20.82.37.12',
     'http://bhd1roots.com',
