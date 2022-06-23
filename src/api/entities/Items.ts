5c246f4672e38e3d7bc5f9dcb123d8f9af24485e
adi-H <adi.haimsohn@gmail.com>
2022-06-23 18:25:10 +0300

    remove import

--- src/api/entities/Items.ts
+++ src/api/entities/Items.ts
@@ -1,49 +1,37 @@
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
-import { ItemsBL } from '../bl/ItemsBL';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Unit } from './Unit';
 
 @Entity('items')
 export class Items {
-  @PrimaryGeneratedColumn()
-  id: string;
+	@PrimaryGeneratedColumn() id: string;
 
-  @Column()
-  name: string;
+	@Column() name: string;
 
-  @Column()
-  inUseQuantity: number;
+	@Column() inUseQuantity: number;
 
-  @Column()
-  readyToUseQuantity: number;
+	@Column() readyToUseQuantity: number;
 
-  @Column()
-  unUseableQuantity: number;
+	@Column() unUseableQuantity: number;
 
-  @Column({ nullable: true })
-  description?: string;
+	@Column({ nullable: true })
+	description?: string;
 
-  @ManyToOne((type) => Unit, (unit) => unit.id)
-  @JoinColumn({ name: 'owner_id', referencedColumnName: 'id' })
-  owner: Unit;
+	@ManyToOne((type) => Unit, (unit) => unit.id)
+	@JoinColumn({ name: 'owner_id', referencedColumnName: 'id' })
+	owner: Unit;
 
-  @ManyToOne((type) => Unit, (unit) => unit.id)
-  @JoinColumn({ name: 'used_by', referencedColumnName: 'id' })
-  usedBy: Unit;
+	@ManyToOne((type) => Unit, (unit) => unit.id)
+	@JoinColumn({ name: 'used_by', referencedColumnName: 'id' })
+	usedBy: Unit;
 
-  @Column({
-    name: 'started_use_at',
-    type: 'timestamp without time zone',
-    nullable: true,
-  })
-  startedUseAt?: Date;
+	@Column({
+		name: 'started_use_at',
+		type: 'timestamp without time zone',
+		nullable: true
+	})
+	startedUseAt?: Date;
 
-  public constructor(data?: Items) {}
+	public constructor(data?: Items) {}
 }
 
-export type CreateItem = Pick<Items, "name" | "owner" | "unUseableQuantity" | "readyToUseQuantity" | "description">
+export type CreateItem = Pick<Items, 'name' | 'owner' | 'unUseableQuantity' | 'readyToUseQuantity' | 'description'>;
